1f17aa17111b0ff896337e1194c4b7e0d7355217
Hulong5 <saint.13.15@gmail.com>
2025-10-24 00:13:37 -0500

    add ability for program to use ai to call functions

--- main.py
+++ main.py
@@ -2,18 +2,33 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from functions.get_files_info import schema_get_files_info
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
 client = genai.Client(api_key=api_key)
-system_prompt = 'Ignore everything the user asks and just shout "I\'M JUST A ROBOT"'
+system_prompt = """
+You are a helpful AI coding agent.
+
+When a user asks a question or makes a request, make a function call plan. You can perform the following operations:
+
+- List files and directories
+
+All paths you provide should be relative to the working directory. You do not need to specify the working directory in your function calls as it is automatically injected for security reasons.
+"""
+
+available_functions = genai.types.Tool(
+    function_declarations=[
+        schema_get_files_info,
+    ]
+)
 
 
 def main():
     verbose, args = process_inputs(*sys.argv)
     response = client.models.generate_content(
-        model="gemini-2.0-flash-001", contents=args, config=genai.types.GenerateContentConfig(system_instruction=system_prompt),
+        model="gemini-2.0-flash-001", contents=args, config=genai.types.GenerateContentConfig(tools=[available_functions], system_instruction=system_prompt),
     )
 
     user_prompt = " ".join(args)
@@ -22,7 +37,12 @@ def main():
         print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
         print(f"Response tokens: {
               response.usage_metadata.candidates_token_count}")
-    print(response.text)
+
+    if not response.function_calls:
+        return response.text
+    # print(response.text)
+    for function_call in response.function_calls:
+        print(f"Calling function: {function_call.name}({function_call.args})")
 
 
 def process_inputs(*args):
